65c6559ffc3e3ce0f9909f97500afec3d1aaed7a
aariadnavs <ariadnavsantillan@gmail.com>
2026-05-15 19:16:27 -0300

    Primer Commit. Creacion de carpetas ejem01, ejem02 y ejem 03. También de README

---
+++ README.md
@@ -0,0 +1,40 @@
+# Actividad DAD
+Fecha: 15-05-2026
+Alumna: Ariadna Santillan
+
+## Consigna 1: ejem01
+Editar dentro del contenedor de Docker
+- Instalar un editor de texto como Vim o Nano
+- Para instalar en linux un programa se debe actualizar el listado de repositorios de APT
+- Luego ejecutar por ej para instalar Vim, el comando apt-get install -y vim
+- Es probable que se deba corregir el Dockerfile, pegar el error en, por ej. Chatgpt.
+- Editar el archivo index.html con Vi desde la terminal del docker ejecutando vi index.html.
+- poner su nombre, fecha y materia. Para editar presionar por primera vez la letra i. Para guardar presionar ESC, luego :, luego wq.
+- Hacer captura de pantalla y pega en el Readme del github q creen para éste proyecto 
+ 
+ ## Respuestas
+Captura de pantalla de editor de index.html en terminal
+
+![Ejercicio realizado desde terminal](<images/ejem01/index editado desde terminal.png>)
+
+Cambios demostrados en el html
+
+![HTML con mi nombre, fecha y materia](<images/ejem01/index editado.png>)
+
+## Consigna 2: ejem02
+
+- run.sh es un archivo para ejecutar en una terminal de linux sh o bash
+- Interpretar los comandos en run.sh
+- Ejecutar manualmente en la terminal de VS Code
+- Subir captura de pantalla del despliegue de docker
+
+Captura de wordpress funcionando
+
+![Captura de pantalla de Wordpress](<images/ejem02/wordpress ejem02.png>)
+
+## Consigna 3: ejem03
+
+- Correr el script run.sh del ejem03
+- Evaluar inconvenientes de correr scripts de S.O.
+
+
---
+++ ejem02/wordpress/wp-config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'manager');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'secret');
+
+/** MySQL hostname */
+define('DB_HOST', 'mysql');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '1cf99d677fdd37279cbaece234b479905286c5a4');
+define('SECURE_AUTH_KEY',  '6fb6883ed67575190d10401e5f730265adaed1cc');
+define('LOGGED_IN_KEY',    '17f4a9c1ad7a35cf7737dde5d556c39b50894773');
+define('NONCE_KEY',        'd845179b0b83abaeda134107da4a7de364b08f8d');
+define('AUTH_SALT',        'a0d1df9f18ae9802bc11f18e7013c9f19324c6ee');
+define('SECURE_AUTH_SALT', '61423f5d71188aa52640fcb87f5ae7fd422b46dc');
+define('LOGGED_IN_SALT',   '68e36938a1d5c61e3a1f6d056895d7344c531164');
+define('NONCE_SALT',       '183540625198f793b454f43c8a0796de4eff8c8d');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
